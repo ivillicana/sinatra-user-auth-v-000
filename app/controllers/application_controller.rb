5834bd7a1e78f089990bab962f700ece0fd78072
Isaac Villicana <villicana.i@gmail.com>
2018-06-27 14:06:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(params)
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
     redirect '/users/home'
